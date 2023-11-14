19961ea9c40c4e341a94c748be793d5dfa4c1b70
mtamas-github <51745124+mtamas-github@users.noreply.github.com>
2023-11-14 11:18:57 +0100

    Update AlgoConnect.py

--- apps/home/AlgoConnect.py
+++ apps/home/AlgoConnect.py
@@ -1,6 +1,7 @@
 
 from algosdk.v2client import algod
 
+# move these values into .env after testing proved
 DEFAULT_ADDRESS = "https://academy-algod.dev.aws.algodev.network/"
 DEFAULT_TOKEN = "2f3203f21e738a1de6110eba6984f9d03e5a95d7a577b34616854064cf2c0e7b"
 
